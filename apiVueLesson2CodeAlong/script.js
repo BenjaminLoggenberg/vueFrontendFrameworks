c37a9db16b71af9a2bdb5cc4d1ff37b834e3cb14
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-08-25 13:54:47 +0200

    filter vue list
    Filtering Vue list using Computed

--- apiVueLesson2CodeAlong/script.js
+++ apiVueLesson2CodeAlong/script.js
@@ -33,27 +33,36 @@ const getData = () => new Promise((resolve) => {
         .then(response => response.json())
         .then(json => json.data.map(item => item.name))
         .then(names => resolve(names))
+
 })
 
 const component = {
     data() {
         return {
-            list: [
-                { name: 'Jan' },
-                { name: 'Sannie' },
-                { name: 'Koos' },
-                { name: 'Peter' },
-            ],
+            list: [],
+            search: '',
+        }
+    },
+    computed: {
+        filteredList() { //HOW TO FILTER IN Vue
+            return this.list.filter(item => item.includes(this.search))
         }
     },
 
     mounted() {
-
+        console.log("im mounted")
+        getData().then(resolveData => { this.list = resolveData })
     },
 
-    template: /*HTML, first line displays something while loading */ `
+    template: /*HTML - first line cool tip for while data loading*/`
     <div v-if="list.length < 1">Fetching data...</div> 
-    <div v-for="item in list"> {{ item.name }}</div>
+    <div v-else>
+        <input v-model="search">
+        <div> {{ search }} </div>
+            <ul>
+                 <li v-for="item in filteredList"> {{ item }} </li>
+             </ul>
+        </div>
     `
 }
 
